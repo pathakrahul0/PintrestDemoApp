35b26eed38348ed8f4490998e3e35660304ea6e2
Rahul <rahulpathak.ind@gmail.com>
2020-02-18 15:08:49 +0530

    first commit

--- app/src/main/java/com/pintrestdemoapp/MainActivity.java
+++ app/src/main/java/com/pintrestdemoapp/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(PDKException exception) {
                 Log.e(getClass().getName(), exception.getDetailMessage());
             }
+
+
         });
     }
 
