a81141fc239bbf67a76ce8a3557fa439411b51c3
Rahul <rahulpathak.ind@gmail.com>
2020-02-18 14:57:32 +0530

    first commit

---
+++ app/src/main/java/com/pintrestdemoapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.pintrestdemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.pinterest.android.pdk.PDKCallback;
+import com.pinterest.android.pdk.PDKClient;
+import com.pinterest.android.pdk.PDKException;
+import com.pinterest.android.pdk.PDKResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PDKClient pdkClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pdkClient = PDKClient.configureInstance(this, "5082518573490949609");
+        pdkClient.onConnect(this);
+        List<String> scopes = new ArrayList<>();
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_READ_PUBLIC);
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_WRITE_PUBLIC);
+
+        pdkClient.login(this, scopes, new PDKCallback() {
+            @Override
+            public void onSuccess(PDKResponse response) {
+                Log.d(getClass().getName(), response.getData().toString());
+                //user logged in, use response.getUser() to get PDKUser object
+            }
+
+            @Override
+            public void onFailure(PDKException exception) {
+                Log.e(getClass().getName(), exception.getDetailMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        pdkClient.onOauthResponse(requestCode, resultCode, data);
+    }
+
+}
